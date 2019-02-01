ec7b489a3b104790a66095b472964a5324ea6cc4
Jorge Moura <jjorgeammoura@gmail.com>
2019-02-01 09:49:38 -0500

    Episode 21: Project Activity Feeds Part 2

--- tests/Feature/ActivityFeedTest.php
+++ tests/Feature/ActivityFeedTest.php
@@ -12,7 +12,8 @@ class ActivityFeedTest extends TestCase
 	use RefreshDatabase;
 
 	/** @test */
-	public function creating_a_project_generates_activity(){
+	public function creating_a_project_records_activity(){
+		$this->withoutExceptionHandling();
 		$project = ProjectFactory::create();
 
 		$this->assertCount(1, $project->activity);
@@ -20,12 +21,35 @@ class ActivityFeedTest extends TestCase
 	}
 
 	/** @test */
-	public function updating_a_project_generates_activity(){
+	public function updating_a_project_records_activity(){
 		$project = ProjectFactory::create();
 
 		$project->update(['title' => 'Changed']);
 
 		$this->assertCount(2, $project->activity);
 		$this->assertEquals('updated', $project->activity->last()->description);
+	}
+
+	/** @test */
+	public function creating_a_new_task_records_project_activity(){
+		$project = ProjectFactory::create();
+
+		$project->addTask('Some task');
+
+		$this->assertCount(2, $project->activity);
+		$this->assertEquals('created_task', $project->activity->last()->description);
+	}
+
+	/** @test */
+	public function completing_a_new_task_recordes_project_activity(){
+		$project = ProjectFactory::withTasks(1)->create();
+
+		$this->actingAs($project->owner)->patch($project->tasks[0]->path(), [
+			'body' => 'foobar',
+			'completed' => true
+		]);
+
+		$this->assertCount(3, $project->activity);
+		$this->assertEquals('completed_task', $project->activity->last()->description);
 	}
 }
