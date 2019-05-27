9d6db59940cce31e7887f7430e64b353006fd8d2
Jorge Moura <jjorgeammoura@gmail.com>
2019-05-27 18:09:28 -0500

    Episode 33, 34: Inviting Users as a Feature Test. Validation Errors For Multiple Forms

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -13,7 +13,53 @@ class InvitationsTest extends TestCase
 	use RefreshDatabase; 
 
 	/** @test */
-	public function a_project_can_invite_a_user(){
+	public function non_owners_may_not_invite_users(){
+		$project = ProjectFactory::create();
+		$user = factory(User::class)->create();
+
+		$assertInvitationForbidden = function() use($user, $project) {
+			$this->actingAs($user)
+				->post($project->path().'/invitations')
+				->assertStatus(403);
+		};
+
+		$assertInvitationForbidden();
+
+		$project->invite($user);
+
+		$assertInvitationForbidden();
+
+	}
+
+	/** @test */
+	public function a_project_owner_can_invite_a_user(){
+		$this->withoutExceptionHandling();
+		
+		$project = ProjectFactory::create();
+		$userToInvite = factory(User::class)->create();
+
+		$this->actingAs($project->owner)->post($project->path().'/invitations', [
+			'email' => $userToInvite->email
+		]);
+
+		$this->assertTrue($project->members->contains($userToInvite));
+	}
+
+	/** @test */
+	public function the_email_address_must_be_associated_with_a_valid_birdboard_account(){
+		$project = ProjectFactory::create();
+
+		$this->actingAs($project->owner)
+			->post($project->path().'/invitations', [
+				'email' => 'notuser@example.com'
+			])
+			->assertSessionHasErrors([
+				'email' => 'The user you are inviting must have a Birdboard account.'
+			], null, 'invitations');
+	}
+
+	/** @test */
+	public function invited_users_may_update_project_details(){
 		$project = ProjectFactory::create();
 
 		$project->invite($newUser = factory(User::class)->create());
